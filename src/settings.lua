52bed9a312c2df3f7021c9b3fd59566924de7a9a
Phoniez <66821881+PhonieZ@users.noreply.github.com>
2023-01-02 14:04:04 +0000

    Added Mod Settings

---
+++ src/settings.lua
@@ -0,0 +1,10 @@
+data:extend({--  Mod Settings
+
+    {--  Should Arrows On Pipe Junctions Be Disabled?
+        name="se-flow-bridge-is-pipe-arrows-disabled",
+        type="bool-setting",
+        setting_type="startup",
+        default_value=true
+    }
+
+})
